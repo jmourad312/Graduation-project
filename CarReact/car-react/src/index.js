8e9c516631562d0e0fa15f54307705d6c83d1ab0
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-01-28 08:32:33 +0200

    styless.scss is now working and migrated html to jsx components

--- CarReact/car-react/src/index.js
+++ CarReact/car-react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles.scss';
 
 // import 'bootstrap/dist/css/bootstrap.css';
 // import 'bootstrap/scss/bootstrap.scss';
