e93e3397dccf0bd7be5246f055b7b14f212f9f2f
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-01-29 12:56:56 +0200

    New homepage design and finish info pages

--- CarReact/car-react/src/App.js
+++ CarReact/car-react/src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import Navbar from "./layout/navbar/Navbar.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes/Routes";
+import Homepage from "./layout/Homepage/Homepage";
+import Homepage2 from "./layout/Homepage/Homepage2";
+
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <Routes/>
+        <Homepage2/>
+        {/* <Routes/> */}
       </Router>
     </div>
   );
