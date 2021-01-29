7a9ab7aaf07ff9a8ca7b55c9a706db683c8af0a7
noraalaa <66481339+noraalaa@users.noreply.github.com>
2021-01-29 16:33:36 +0200

    n

--- authNodeJS/app.js
+++ authNodeJS/app.js
@@ -2,11 +2,19 @@ const express = require("express");
 const passport = require("passport");
 const bodyparser = require("body-parser");
 const db = require('./config/configDB')
-const AuthUserRoute = require('./routes/user/auth');
 // to run at angular
 const cors = require("cors");
 //generate key private and public for secret token
 const genKeyPair = require("./generatekeypair");
+//Routes 
+const AuthUserRoute = require('./routes/user/auth');
+const AuthVendorRoute = require('./routes/vendor/auth');
+const AuthAdminRoute = require('./routes/admin/auth');
+
+const UserRoute = require('./routes/user/user');
+const VendorRoute = require('./routes/vendor/vendor');
+const AdminRoute = require('./routes/admin/admin');
+
 
 
 //server
@@ -24,7 +32,15 @@ require("./config/passport")(passport);
 app.use(passport.initialize());
 
 //routes
-app.use("/auth",AuthUserRoute);
+app.use("/user/auth",AuthUserRoute);
+app.use("/user",UserRoute);
+
+//app.use("/vendor/auth",AuthVendorRoute);
+//app.use("/vendor",VendorRoute);
+
+//app.use("/admin/auth",AuthAdminRoute);
+//app.use("/admin",AdminRoute);
+
 
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
