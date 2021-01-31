33cbcf3f3e06f52d8a941ef188d67003635626ba
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-01-31 10:50:13 +0200

    added routing

--- CarReact/car-react/src/routes/Routes.jsx
+++ CarReact/car-react/src/routes/Routes.jsx
@@ -2,15 +2,52 @@ import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 
 // const AuthRoutes = React.lazy(() => import("../pages/user/authentication/AuthRoutes"));
-const Homepage2 = React.lazy(() =>
-  import("../layout/Homepage/Homepage2")
-);
+const Homepage2 = React.lazy(() => import("../layout/Homepage/Homepage2"));
 
 const Login = React.lazy(() =>
   import("../pages/user/authentication/login/Login")
 );
 
-// const About = React.lazy(() => import("../Pages/About"));
+const SignUp = React.lazy(() =>
+  import("../pages/user/authentication/signUp/SignUp")
+);
+const ResetPassword = React.lazy(() =>
+  import("../pages/user/authentication/resetPassword/ResetPassword")
+);
+const AddBlog = React.lazy(() =>
+  import("../pages/user/blog/addBlog/AddBlog.jsx")
+);
+const BlogDetails = React.lazy(() =>
+  import("../pages/user/blog/blogDetails/BlogDetails.jsx")
+);
+const BlogList = React.lazy(() =>
+  import("../pages/user/blog/blogList/BlogList")
+);
+const AboutUs = React.lazy(() => 
+  import("../pages/user/info/aboutUs/AboutUs.jsx")
+);
+const ContactUs = React.lazy(() =>
+  import("../pages/user/info/contactUs/ContactUs")
+);
+const Privacy = React.lazy(() => import("../pages/user/info/privacy/Privacy"));
+const TermsOfService = React.lazy(() =>
+  import("../pages/user/info/termsOfService/TermsOfService")
+);
+const VendorProfileUser = React.lazy(() =>
+  import("../pages/user/info/vendor/VendorProfileUser")
+);
+const ProductDetails = React.lazy(() =>
+  import("../pages/user/product/productDetails/ProductDetails")
+);
+const ProductsList = React.lazy(() =>
+  import("../pages/user/product/productList/ProductsList")
+);
+const DisplayUserProfile = React.lazy(() =>
+  import("../pages/user/profile/displayProfile/DisplayUserProfile")
+);
+
+
+
 // const Contact = React.lazy(() => import("../Pages/Contact"));
 // const Counter = React.lazy(() => import("../Pages/Counter"));
 // const Users = React.lazy(() => import("../Pages/Users"));
@@ -24,7 +61,23 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Homepage2} />
         <Route path="/login" exact component={Login} />
-        
+        <Route path="/SignUp" exact component={SignUp} />
+        <Route path="/ResetPassword" exact component={ResetPassword} />
+        <Route path="/AddBlog" exact component={AddBlog} />
+        <Route path="/BlogDetails" exact component={BlogDetails} />
+        <Route path="/BlogList" exact component={BlogList} />
+        <Route path="/AboutUs" exact component={AboutUs} />
+        <Route path="/ContactUs" exact component={ContactUs} />
+        <Route path="/Privacy" exact component={Privacy} />
+        <Route path="/TermsOfService" exact component={TermsOfService} />
+        <Route path="/VendorProfileUser" exact component={VendorProfileUser} />
+        <Route path="/ProductDetails" exact component={ProductDetails} />
+        <Route path="/ProductsList" exact component={ProductsList} />
+        <Route
+          path="/DisplayUserProfile"
+          exact
+          component={DisplayUserProfile}
+        />
 
         {/* <Route path="/about" exact component={About} />
         <Route path="/contact" exact component={Contact} />
