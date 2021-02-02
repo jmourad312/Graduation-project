633de32340a585654e79fd359300ed340a2afe73
noraalaa <66481339+noraalaa@users.noreply.github.com>
2021-02-02 20:13:06 +0200

    back2

---
+++ CarReact/GP-Backend/routes/user/user.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const userProfileCtrl = require("../../controller/User/userProfile-ctrl");
+const userBlogCtrl = require("../../Controller/User/userBlog-ctrl");
+
+function canView(req, resp, next) {
+  const { role } = req.user;
+  if (!(role == "user" || role == "admin")) {
+    resp.json({
+      Data: null,
+      Message: "can't access",
+      Success: false,
+    });
+  } else next();
+}
+
+function validateUser(req, resp, next) {
+  const { role, _id } = req.user;
+  if (!((role == "user" || role == "admin") && _id == req.params.id)) {
+    resp.json({
+      Data: null,
+      Message: "can't access",
+      Success: false,
+    });
+  } else next();
+}
+
+// user routes on his profile 
+router.get(
+  "/showUserProfile/:id",
+  passport.authenticate("jwt", { session: false }),
+  validateUser,
+  userProfileCtrl.showUserProfile
+);
+
+router.put("/updateUserPassword/:id",passport.authenticate('jwt', { session: false }),validateUser,userProfileCtrl.updateUserPassword);
+
+
+// user routes on Blog
+router.post(
+  "/addPost/:id",
+  passport.authenticate("jwt", { session: false }),
+  validateUser,
+  userBlogCtrl.addNewPost
+);
+router.delete(
+  "/deletePost/:id",
+  passport.authenticate("jwt", { session: false }),
+  validateUser,
+  userBlogCtrl.deletePost
+);
+router.put(
+  "/updatePost/:id",
+  passport.authenticate("jwt", { session: false }),
+  validateUser,
+  userBlogCtrl.updatePost
+);
+
+module.exports = router;
