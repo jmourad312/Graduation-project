c9a22f85a0cfa8465ef159ceeb5b62f64f5d3e11
noraalaa <66481339+noraalaa@users.noreply.github.com>
2021-02-02 22:16:15 +0200

    add

--- GP-Backend/models/Blog/post.js
+++ GP-Backend/models/Blog/post.js
@@ -2,30 +2,6 @@ const mongoose = require("mongoose");
 
 var schema = mongoose.Schema;
 var post = new schema({
-<<<<<<< HEAD
-    title: String,
-    body: String,
-    image: [],
-    createdAT: Date,
-    updatedAT: Date,
-    person: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Person",
-    },
-    updatedPosts: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Post"
-        }
-      ],
-      comment: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Reply"
-        }
-      ],
-});
-=======
   title: String,
   body: String,
   image: [],
@@ -54,7 +30,6 @@ var post = new schema({
 }
   , { timestamps: true },
 );
->>>>>>> 8b854073a3efd5023ea63c54a29db7a4d718180b
 
 
 module.exports = mongoose.model("Post", post);
--- GP-Backend/models/Blog/reply.js
+++ GP-Backend/models/Blog/reply.js
@@ -2,30 +2,6 @@ const mongoose = require("mongoose");
 
 var schema = mongoose.Schema;
 var reply = new schema({
-<<<<<<< HEAD
-  content:{
-        type:"String",
-        default:""
-  } 
-  image: [String],
-  createdAT: Date,
-  updatedAT: Date,
-  commentVoting: Number,
-  person: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Person",
-  },
-  post: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Post",
-  },
-  commentReply: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Reply",
-    },
-  ],
-=======
     content: String,
     image: [String],
     commentVoting: Number,
@@ -35,12 +11,9 @@ var reply = new schema({
           ref: "Reply"
         }
       ],
-   
 }
 , { timestamps: true },
 );
->>>>>>> 8b854073a3efd5023ea63c54a29db7a4d718180b
 
-});
 
 module.exports = mongoose.model("Reply", reply);
