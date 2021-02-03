98ba7e7018c94f82bca20d91043a6b58b9800e89
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-02-03 14:43:44 +0200

    product filter update

--- CarReact/car-react/src/components/ProductFilter.jsx
+++ CarReact/car-react/src/components/ProductFilter.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Dropdown from './Dropdown'
+import cars2 from '../assets/js/cars2';
+import cars from "../assets/js/cars";
+
 
 export default function ProductFilter() {
     const carBrand = ["BMW", "AUDI", "MAZARATI", "HYUNDAI"];
@@ -8,14 +11,14 @@ export default function ProductFilter() {
     return (
       <div className="productFilter">
         <div className="row">
-          <Dropdown mapItems={carBrand} name="brand" />
+          <Dropdown mapItems={cars2} name="brand" />
         </div>
-        <div className="row mt-2">
+        {/* <div className="row mt-2">
           <Dropdown mapItems={carBrand2} name="model" />
         </div>
         <div className="row mt-2">
           <Dropdown mapItems={carBrand3} name="sda" />
-        </div>
+        </div> */}
       </div>
     );
 }
