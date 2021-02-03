6f65a21cbcc5d8d6bb00d28b54f96fbd108b78b1
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-02-03 10:11:47 +0200

    update product list

--- CarReact/car-react/src/pages/user/product/productList/ProductsList.jsx
+++ CarReact/car-react/src/pages/user/product/productList/ProductsList.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
-import ProductComp from '../../../../components/ProductComp'
-import ProductFilter from '../../../../components/ProductFilter'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import ProductComp from '../../../../components/ProductComp';
+import ProductFilter from '../../../../components/ProductFilter';
+import { getProductsAction } from '../../../../store/actions';
+
 
 export default function ProductsList() {
+  
+  const products = useSelector((state) => state.products);
+  console.log(products);
+  const dispatch = useDispatch();
+  console.log(products);
+  const getProducts = () => {
+    dispatch(getProductsAction());
+  };
+  console.log(products);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  const createProducts = (prod) =>{
+    return (
+      <ProductComp
+        key={prod.id}
+        image={prod.image}
+        price={prod.price}
+        title={prod.title}
+        brand={prod.description}
+        model={prod.description}
+        category={prod.category}
+      />
+    );
+  }
   return (
     <div className="productList">
       <div className="container">
@@ -11,7 +40,9 @@ export default function ProductsList() {
             <ProductFilter />
           </div>
           <div className="col-8" style={{ marginTop: "15%" }}>
-            <ProductComp/>
+          <div className="row">
+            {products.map(createProducts)}
+          </div>
           </div>
         </div>
     </div>
