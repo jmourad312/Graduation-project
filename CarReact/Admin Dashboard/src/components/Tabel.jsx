e415059876530cf66cedb231b0742542074bb93a
hajarshalby20 <71436369+hajarshalby20@users.noreply.github.com>
2021-02-10 12:56:13 +0200

    pull

--- CarReact/Admin Dashboard/src/components/Tabel.jsx
+++ CarReact/Admin Dashboard/src/components/Tabel.jsx
@@ -1,60 +1,6 @@
 export function Tabel(props) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  console.log(props.data);
-  return (
-    <>
-      <div className="row wow fadeIn">
-        <div className="col-md-12 mb-4">
-          <div className="card">
-            <div className="card-body">
-              <table className="table table-hover">
-                <thead className="blue-grey lighten-4">
-                  <tr>
-                    <th>firstName</th>
-                    <th>Email</th>
-                    <th>Banned</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                    <th>Ban</th>
-                  </tr>
-                </thead>
 
-                <tbody>
-                  {props.data.map((item, index) => {
-                    return (
-                      <tr>
-                        <td>{item.person._id}</td>
-                        <td>{item.person.firstName}</td>
-                        <td>{item.person.email}</td>
-                        <td>{item.banned}</td>
-                        <td>
-                          <i
-                            style={{ fontSize: "20px" }}
-                            className="fas fa-pen ml-1"
-                          ></i>
-                        </td>
-                        <td>
-                          <i
-                            style={{ fontSize: "20px" }}
-                            className="fas fa-trash ml-3"
-                          ></i>
-                        </td>
-                        <td>
-                          <i
-                            style={{ fontSize: "20px" }}
-                            className="fas fa-ban"
-                          ></i>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-=======
-=======
-    console.log(props.data)
->>>>>>> 2339a20f6c6c8d0d53ae64767480866e21526c68
+  console.log(props.data);
     return (
         <>
             <div className="row wow fadeIn" id={props.id}>
@@ -62,7 +8,6 @@ export function Tabel(props) {
                     <div className="card">
                         <div className="card-body">
                             <table className="table table-hover">
-
 
                                 <thead className="blue-grey lighten-4">
                                     <tr>
@@ -100,10 +45,11 @@ export function Tabel(props) {
                         </div>
                     </div>
                 </div>
->>>>>>> b0ed402652b4fe695b1aca4326c8d1045a6638b7
+
             </div>
           </div>
         </div>
+     
       </div>
     </>
   );
