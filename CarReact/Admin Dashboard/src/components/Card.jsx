c0b1c4ae26cd52680066cf3cae792666b6bdb13f
hajarshalby20 <71436369+hajarshalby20@users.noreply.github.com>
2021-02-11 19:47:05 +0200

    pulling

--- CarReact/Admin Dashboard/src/components/Card.jsx
+++ CarReact/Admin Dashboard/src/components/Card.jsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import { Button } from "../components/Button";
-=======
 import { Button } from '../components/Button'
 import { instance } from "../network/axiosConfig";
 
->>>>>>> 68da189e017e17ac92c566b06c3e066c8a5cac46
 
 export function Card(props) {
   return (
@@ -51,30 +47,6 @@ export function Card(props) {
             </small>
           </div>
 
-<<<<<<< HEAD
-          <div className="d-flex">
-            <Button
-              className="btn btn-info mx-auto m-2"
-              parameter={props.id}
-              key={props.id}
-              handelClick={props.handelClickEdit}
-              name={<i style={{ fontSize: "20px" }} className="fas fa-pen"></i>}
-            ></Button>
-
-            <Button
-              className="btn btn-danger mx-auto m-2"
-              parameter={props.id}
-              key={props.id}
-              handelClick={props.handelClickDelete}
-              name={
-                <i style={{ fontSize: "20px" }} className="fas fa-trash"></i>
-              }
-            ></Button>
-          </div>
-        </article>
-      </section>
-    </div>
-=======
     <div className="col-4 productComp mb-2 blog-post" >
         <section className="cards">
           <article className="card card--1">
@@ -108,6 +80,5 @@ export function Card(props) {
           </article>
         </section>
       </div>
->>>>>>> 68da189e017e17ac92c566b06c3e066c8a5cac46
   );
 }
