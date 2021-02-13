3f6abd210f15a6ab53ccd50fdfa36d6e5462e53d
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-02-13 14:01:35 +0200

    Update ContactUs.jsx

--- CarReact/car-react/src/pages/user/info/contactUs/ContactUs.jsx
+++ CarReact/car-react/src/pages/user/info/contactUs/ContactUs.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import ToastMessage from "../../../../components/ToastMessage";
 
 export default function ContactUs() {
   
@@ -17,43 +18,93 @@ export default function ContactUs() {
       };
     });
   }
-
-  const handleSubmit = () => {
-    const config = {
-      headers: {
-        Authorization: localStorage.getItem("Authorization"),
-      },
-    };
+  
+  const [toastStatus, setToastStatus] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+  const toggleStatus = () => {
+    setToastStatus(true);
+    setTimeout(() => {
+      setToastStatus(false);
+    }, 2000);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // const config = {
+    //   headers: {
+    //     Authorization: localStorage.getItem("Authorization"),
+    //   },
+    // };
 
     const body = {
-      car: localStorage.getItem("ProductID"),
-      // comment: userFeedbackInfo.comment,
+      email:contactInput.email,
+      message:contactInput.content,
     };
-
-    const URL = "http://localhost:3000/user/writeFeedback";
+    console.log(body);
+    const URL = "http://localhost:3000/admin/sendContact";
 
     axios
-      .post(URL, body, config)
+      .post(URL, body)
       .then((req) => {
+        console.log(body);
         console.log(req);
         if (req.data.Success === true) {
-          console.log("Success");
+          setToastMessage(
+            "Thank you for your message,\nWe will contact you as soon as possible"
+          );
+          toggleStatus();
+          // console.log("Success");
         } else {
-          console.log("fail");
+          setToastMessage(
+            "Sorry an error occured, please try again later"
+          );
+          toggleStatus();
         }
       })
       .catch((error) => {
         console.log(error);
       });
+      setContactInput({
+        email:"",
+        content:""
+      })
   };
 
   return (
     <section className="contact-us">
-      <section  className="content-section text-center">
+    <div style={{position:"absolute",top:"45%",right:"29%",height:"100px",width:"300px"}}>
+      <ToastMessage
+        showFunction={toggleStatus}
+        status={toastStatus}
+        message={toastMessage}
+      />
+    </div>
+      <section className="content-section text-center">
         <div className="contact-section">
-          <div className="container" >
-            <h1 style={{ fontWeight: "700",fontSize:"45px",fontFamily:"cursive",backgroundImage: "linear-gradient(to top, #08091d 0%, #a2a5a8 90%)",color:"transparent",WebkitBackgroundClip:"text" }}>Contact Us</h1>
-            <p style={{ fontWeight: "600",fontSize:"35px",fontFamily:"cursive",backgroundImage: "linear-gradient(to top, #08091d 0%, #a2a5a8 90%)",color:"transparent",WebkitBackgroundClip:"text" }}>
+          <div className="container">
+            <h1
+              style={{
+                fontWeight: "700",
+                fontSize: "45px",
+                fontFamily: "cursive",
+                backgroundImage:
+                  "linear-gradient(to top, #08091d 0%, #a2a5a8 90%)",
+                color: "transparent",
+                WebkitBackgroundClip: "text",
+              }}
+            >
+              Contact Us
+            </h1>
+            <p
+              style={{
+                fontWeight: "600",
+                fontSize: "35px",
+                fontFamily: "cursive",
+                backgroundImage:
+                  "linear-gradient(to top, #08091d 0%, #a2a5a8 90%)",
+                color: "transparent",
+                WebkitBackgroundClip: "text",
+              }}
+            >
               Feel free to contact us anytime
             </p>
             <div className="row">
@@ -61,7 +112,20 @@ export default function ContactUs() {
               <div className="col-8">
                 <form className="form">
                   <div className="form-group">
-                    <label for="Email2" style={{ fontWeight: "600", fontSize: "30px",fontFamily:"cursive",backgroundImage: "linear-gradient(to top, #08091d 0%, #a2a5a8 100%)",color:"transparent",WebkitBackgroundClip:"text" }}>Email</label>
+                    <label
+                      for="Email2"
+                      style={{
+                        fontWeight: "600",
+                        fontSize: "30px",
+                        fontFamily: "cursive",
+                        backgroundImage:
+                          "linear-gradient(to top, #08091d 0%, #a2a5a8 100%)",
+                        color: "transparent",
+                        WebkitBackgroundClip: "text",
+                      }}
+                    >
+                      Email
+                    </label>
                     <input
                       type="email"
                       className="form-control"
@@ -73,7 +137,20 @@ export default function ContactUs() {
                     />
                   </div>
                   <div className="form-group ">
-                    <label for="exampleInputText" style={{ fontWeight: "600", fontSize: "25px",fontFamily:"cursive",backgroundImage: "linear-gradient(to top, #08091d 0%, #a2a5a8 90%)",color:"transparent",WebkitBackgroundClip:"text" }}>Your Message</label>
+                    <label
+                      for="exampleInputText"
+                      style={{
+                        fontWeight: "600",
+                        fontSize: "25px",
+                        fontFamily: "cursive",
+                        backgroundImage:
+                          "linear-gradient(to top, #08091d 0%, #a2a5a8 90%)",
+                        color: "transparent",
+                        WebkitBackgroundClip: "text",
+                      }}
+                    >
+                      Your Message
+                    </label>
                     <textarea
                       className="form-control inputheight"
                       placeholder="Description"
@@ -82,7 +159,12 @@ export default function ContactUs() {
                       value={contactInput.content}
                     ></textarea>
                   </div>
-                  <button type="button" className="btn contrastclass" onClick={handleSubmit} style={{fontWeight:"600",fontSize:"25px"}}>
+                  <button
+                    type="button"
+                    className="btn contrastclass"
+                    onClick={handleSubmit}
+                    style={{ fontWeight: "600", fontSize: "25px" }}
+                  >
                     Send Message
                   </button>
                 </form>
@@ -93,30 +175,45 @@ export default function ContactUs() {
         </div>
         <hr />
 
-        <h3 style={{fontWeight:"600",fontFamily:"cursive",backgroundImage: "linear-gradient(to top, #08091d 0%, #a2a5a8 100%)",color:"transparent",WebkitBackgroundClip:"text"}}>Our Social Sites</h3>
+        <h3
+          style={{
+            fontWeight: "600",
+            fontFamily: "cursive",
+            backgroundImage:
+              "linear-gradient(to top, #08091d 0%, #a2a5a8 100%)",
+            color: "transparent",
+            WebkitBackgroundClip: "text",
+          }}
+        >
+          Our Social Sites
+        </h3>
         <div className="list-inline banner-social-buttons">
-          
-            <a href="#" className="btn btn-lg contrastclass mr-2" style={{fontSize:"30px"}}>
-              <i className="fa fa-twitter text-dark">
-                {" "}
-              </i>
-                <span className="network-name ml-1">Twitter</span>
-            </a>
-          
-            <a href="#" className="btn btn-lg contrastclass mr-2" style={{fontSize:"30px"}}>
-              <i className="fa fa-facebook text-dark">
-                {" "}
-              </i>
-                <span className="network-name ml-1">Facebook</span>
-            </a>
-          
-            <a href="#" className="btn btn-lg contrastclass" style={{fontSize:"30px"}}>
-              <i className="fa fa-youtube-play">
-                {" "}
-              </i>
-                <span className="network-name ml-1">Youtube</span>
-            </a>
-         
+          <a
+            href="#"
+            className="btn btn-lg contrastclass mr-2"
+            style={{ fontSize: "30px" }}
+          >
+            <i className="fa fa-twitter text-dark"> </i>
+            <span className="network-name ml-1">Twitter</span>
+          </a>
+
+          <a
+            href="#"
+            className="btn btn-lg contrastclass mr-2"
+            style={{ fontSize: "30px" }}
+          >
+            <i className="fa fa-facebook text-dark"> </i>
+            <span className="network-name ml-1">Facebook</span>
+          </a>
+
+          <a
+            href="#"
+            className="btn btn-lg contrastclass"
+            style={{ fontSize: "30px" }}
+          >
+            <i className="fa fa-youtube-play"> </i>
+            <span className="network-name ml-1">Youtube</span>
+          </a>
         </div>
       </section>
 
@@ -127,8 +224,12 @@ export default function ContactUs() {
               <div className="card contrastclass pb-1">
                 <div className="card-body text-center">
                   <i className="fa fa-phone fa-2x" aria-hidden="true"></i>
-                  <h4 className="text-uppercase"style={{fontWeight:"700"}}>call us</h4>
-                  <p style={{fontWeight:"700"}}>+8801683615582,+8801750603409</p>
+                  <h4 className="text-uppercase" style={{ fontWeight: "700" }}>
+                    call us
+                  </h4>
+                  <p style={{ fontWeight: "700" }}>
+                    +8801683615582,+8801750603409
+                  </p>
                 </div>
               </div>
             </div>
@@ -136,8 +237,12 @@ export default function ContactUs() {
               <div className="card contrastclass pb-3">
                 <div className="card-body text-center">
                   <i className="fa fa-map-marker fa-2x" aria-hidden="true"></i>
-                  <h4 className="text-uppercase" style={{fontWeight:"700"}}>office loaction</h4>
-                  <address style={{fontWeight:"600"}}>Suite 02, Level 12, Sahera Tropical Center</address>
+                  <h4 className="text-uppercase" style={{ fontWeight: "700" }}>
+                    office loaction
+                  </h4>
+                  <address style={{ fontWeight: "600" }}>
+                    Suite 02, Level 12, Sahera Tropical Center
+                  </address>
                 </div>
               </div>
             </div>
@@ -145,8 +250,12 @@ export default function ContactUs() {
               <div className="card contrastclass">
                 <div className="card-body text-center">
                   <i className="fa fa-globe fa-2x" aria-hidden="true"></i>
-                  <h4 className="text-uppercase"style={{fontWeight:"700"}}>email</h4>
-                  <p style={{fontWeight:"700"}}>http://al.a.noman1416@gmail.com</p>
+                  <h4 className="text-uppercase" style={{ fontWeight: "700" }}>
+                    email
+                  </h4>
+                  <p style={{ fontWeight: "700" }}>
+                    http://al.a.noman1416@gmail.com
+                  </p>
                 </div>
               </div>
             </div>
