99db7bcb91259ef553d50322ddb6bacf190fe2d0
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-02-17 02:25:46 +0200

    Delete CarReact/GP-Backend directory

--- CarReact/GP-Backend/models/CarDetails/ItemCollection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-var schema = mongoose.Schema;
-var itemCollection= new schema({
-    type: String, 
-});
-
-module.exports = mongoose.model("ItemCollection",itemCollection);;
--- CarReact/GP-Backend/routes/Forgetpassword.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const passport = require("passport");
-const password = require('../Controller/forgetPassword')
-
-router.post("/forgetPassword", password.forgetPassword);
-router.post("/resetPassword", password.resetPassword)
-
-module.exports = router;
