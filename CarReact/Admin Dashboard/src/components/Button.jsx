dd9772ab0ed42128ba64cf60aff805f4ba9bf219
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-02-17 02:25:22 +0200

    Delete CarReact/Admin Dashboard directory

--- CarReact/Admin Dashboard/src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-
-export function Button2(props) {
-    return (
-        <>
-            <button style={{zIndex:"100",width:"100%"}} disabled={props.disabled} type="submit" onClick={() => props.handelClick(props.parameter)} key={props.key} className={props.className}>{props.name}</button>
-        </>
-    );
-}
