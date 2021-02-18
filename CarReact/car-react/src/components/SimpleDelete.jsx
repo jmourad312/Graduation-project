d08f47811644ef72269e301cdc0cfd72144ea624
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-02-18 21:35:43 +0200

    Update SimpleDelete.jsx

--- CarReact/car-react/src/components/SimpleDelete.jsx
+++ CarReact/car-react/src/components/SimpleDelete.jsx
@@ -27,7 +27,7 @@ export default function SimpleDelete(props) {
         console.log(req);
         if (req.data.Success === true) {
           console.log("success");
-          if (history.pathname !== "/MyProfile/BlogPosts") {
+          if (history.pathname === "/BlogDetails/:id?") {
             history.push("/BlogList");
           }
         } else {
