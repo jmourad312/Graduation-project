5120bfe9f15c41bbdbee2a7a40054298c39e534e
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-02-19 19:58:23 +0200

    update

--- CarReact/car-react/src/pages/user/authentication/signform/signform.jsx
+++ CarReact/car-react/src/pages/user/authentication/signform/signform.jsx
@@ -62,8 +62,7 @@ export default function Signform(props) {
         }
       })
       .catch((error) => {
-        console.log(error.status);
-        
+        console.log(error);
       });
   };
 
@@ -102,6 +101,7 @@ export default function Signform(props) {
       })
       .catch((error) => {
         console.log(error);
+        alert('Please double check your username or password');
       });
   };
 
