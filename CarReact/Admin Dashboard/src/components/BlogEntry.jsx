bb87873b54092ef90f6ca06d569258a3cc206f1a
Youssef Mourad <75029060+jmourad312@users.noreply.github.com>
2021-02-28 18:22:10 +0200

    update admin files

--- CarReact/Admin Dashboard/src/components/BlogEntry.jsx
+++ CarReact/Admin Dashboard/src/components/BlogEntry.jsx
@@ -145,7 +145,7 @@ export default function BlogEntry(props) {
             </p>
             <div className="card__info">
               <h3 className="card__title text-truncate">{props.cardTitle}</h3>
-              <span className="card__by" style={{fontSize:"20px"}}>
+              <span className="card__by" style={{ fontSize: "20px" }}>
                 by{" "}
                 <span className="card__author" title="author">
                   {props.userName}
@@ -153,42 +153,58 @@ export default function BlogEntry(props) {
               </span>
               <br />
               <small>
-                <i className="badge badge-dark" style={{fontSize:"15px"}}>{props.cardBrand}</i>
-                <i className="badge badge-dark ml-1" style={{fontSize:"15px"}}>{props.cardModel}</i>
+                <i className="badge badge-dark" style={{ fontSize: "15px" }}>
+                  {props.cardBrand}
+                </i>
+                <i
+                  className="badge badge-dark ml-1"
+                  style={{ fontSize: "15px" }}
+                >
+                  {props.cardModel}
+                </i>
               </small>
-              <br/>
-              <Button
-                variant="danger"
-                className="mt-1"
-                style={{fontSize:"20px"}}
-                onClick={handleShowMessage}
-              >
-                {props.dataItem.reportPosts.length} Reports 
-              </Button>
+              <br />
+              {props.dataItem.reportPosts.length > 0 && (
+                <Button
+                  variant="danger"
+                  className="mt-1"
+                  style={{ fontSize: "20px" }}
+                  onClick={handleShowMessage}
+                >
+                  {props.dataItem.reportPosts.length} Reports
+                </Button>
+              )}
             </div>
             <div className="row">
               <div className="col-6">
-
-              <Button
-                variant="primary"
-                className="btn btn-info"
-                style={{ zIndex: "100",width:"120px",marginBottom:"5px",marginLeft:"5px"}}
-                onClick={handleShow}
-              >
-                <i style={{ fontSize: "20px" }} className="fas fa-pen"></i>
-              </Button>
+                <Button
+                  variant="primary"
+                  className="btn btn-info"
+                  style={{
+                    zIndex: "100",
+                    width: "120px",
+                    marginBottom: "5px",
+                    marginLeft: "5px",
+                  }}
+                  onClick={handleShow}
+                >
+                  <i style={{ fontSize: "20px" }} className="fas fa-pen"></i>
+                </Button>
               </div>
               <div className="col-6">
-              <Button2
-                className="btn btn-danger"
-                style={{marginRight:"10px" }}
-                parameter={props.dataItem}
-                key={props.id}
-                handelClick={props.delete}
-                name={
-                  <i style={{ fontSize: "20px"}} className="fas fa-trash"></i>
-                }
-              ></Button2>
+                <Button2
+                  className="btn btn-danger"
+                  style={{ marginRight: "10px" }}
+                  parameter={props.dataItem}
+                  key={props.id}
+                  handelClick={props.delete}
+                  name={
+                    <i
+                      style={{ fontSize: "20px" }}
+                      className="fas fa-trash"
+                    ></i>
+                  }
+                ></Button2>
               </div>
             </div>
           </article>
